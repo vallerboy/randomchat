a5544bd3d64e25279081137073a0f64b773a7619
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-06-18 20:17:06 +0200

    update style.css

--- src/main/java/pl/oskarpolak/randomchat/models/ChatSocket.java
+++ src/main/java/pl/oskarpolak/randomchat/models/ChatSocket.java
@@ -9,13 +9,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 @Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
 
     private List<WebSocketSession> userList = new ArrayList<>();
+    private Queue<String> lastTenMessages = new ArrayDeque<>();
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
@@ -26,6 +29,14 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         userList.add(session);
+
+        lastTenMessages.forEach(s -> {
+            try {
+                session.sendMessage(new TextMessage(s));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -35,6 +46,8 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        addMessageToQue(message.getPayload());
+
         userList.forEach(s -> {
             try {
                 s.sendMessage(new TextMessage(message.getPayload()));
@@ -42,5 +55,13 @@ public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigu
                 e.printStackTrace();
             }
         });
+    }
+
+    private void addMessageToQue(String message){
+        if(lastTenMessages.size() >= 10){
+            lastTenMessages.poll();
+        }
+
+        lastTenMessages.offer(message);
     }
 }
