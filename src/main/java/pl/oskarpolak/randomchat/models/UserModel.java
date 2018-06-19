a000339c4ce9bf883c72a9fc67d4cbd2a485ca28
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-06-19 19:14:02 +0200

    update style.css

---
+++ src/main/java/pl/oskarpolak/randomchat/models/UserModel.java
@@ -0,0 +1,22 @@
+package pl.oskarpolak.randomchat.models;
+
+
+import lombok.Data;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+
+@Data
+public class UserModel {
+    private String nickname;
+    private WebSocketSession userSession;
+
+    public UserModel(WebSocketSession userSession) {
+        this.userSession = userSession;
+    }
+
+    public void sendMessage(TextMessage textMessage) throws IOException {
+        userSession.sendMessage(textMessage);
+    }
+}
